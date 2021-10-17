bbb69444d155f0efe1b705e0ff7bb55938c2f3d9
Michael Andre John T. Lacadin <38705944+dreilacadin@users.noreply.github.com>
2021-10-18 06:27:44 +0800

    Deleting .zshrc

--- install-meslo-fonts.sh
+++ install-meslo-fonts.sh
@@ -1,3 +1,7 @@
+echo -n "This will install  meslo nerd font. Continue? (y/n)? "
+read answer
+
+
 mkdir -p ~/.fonts
 curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf --output ~/.fonts/'MesloLGS NF Regular.ttf'
 curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf --output ~/.fonts/'MesloLGS NF Bold.ttf'
